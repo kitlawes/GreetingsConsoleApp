65013fa06067efce6f8cfd8a91dd2456ce4a2f44
Kit Lawes <kitlawes@hotmail.com>
2017-04-30 14:36:51 +0100

    Add project files.

---
+++ GreetingsConsoleApp/GreetingsConsoleApp.cpp
@@ -0,0 +1,13 @@
+// GreetingsConsoleApp.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>  
+using namespace std;
+
+int main()
+{
+	cout << "Hello\n";
+    return 0;
+}
+
